945617434f5e41a0dbb6ce16f17f643718ab881c
janiello <janiello417@gmail.com>
2019-06-14 15:50:15 -0400

    took out two require methods from server.js to get it running properly. Started adding routes to htmlRoutes.js (catch-all home and survey routes)

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require("express");
 var path = require("path");
 
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-var app = express();
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App is listening on: http://localhost:" + PORT);
 });
+
+module.exports = app;
+module.exports = path;
