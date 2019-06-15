e2866a34cb38bed61149d61c5e3783d543468d9b
janiello <janiello417@gmail.com>
2019-06-14 20:14:37 -0400

    created my array of objects in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+    {
+        name: "Han Solo",
+        scores: [5, 2, 5, 2, 3]
+    }, 
+    {
+        name: "Luke Skywalker",
+        scores: [4, 5, 2, 5, 3]
+    },
+    {
+        name: "Poe Dameron",
+        scores: [4, 3, 3, 4, 1]
+    },
+    {
+        name: "Anakin Skywalker",
+        scores: [5, 1, 5, 2, 2]
+    },
+    {
+        name: "C-3PO",
+        scores: [1, 4, 2, 3, 5]
+    },
+    {
+        name: "Hondo Onaka",
+        scores: [5, 1, 4, 2, 1]
+    }
+];
